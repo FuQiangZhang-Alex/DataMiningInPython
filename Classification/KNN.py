0c47fd56134993d6e52a1a3fc09faf9cbc996d57
zhangfuqiang <zfq_work@163.com>
2016-06-03 14:28:32 +0800

    KNN update

--- Classification/KNN.py
+++ Classification/KNN.py
@@ -14,6 +14,15 @@ labels = {
 }
 
 
+def knn_classify(in_x, data_set, lbl, k):
+    data_set_size = data_set.shape[0]
+    diff_mat = np.tile(in_x, (data_set_size, 1)) - data_set  # difference of input data and all training data
+    square_diff_mat = diff_mat ** 2
+    distance_mat = square_diff_mat ** 0.5
+    sorted_distance = distance_mat.argsort()
+    print(sorted_distance)
+
+
 def file2matrix():
     class_vector = []
     with open(file='data/Activity Recognition from Single Chest-Mounted Accelerometer/debug.csv') as training:
@@ -36,4 +45,22 @@ def draw():
                , c=np.array(class_array) / 10, s=5)
     plt.show()
 
-draw()
+
+def norm_data(data_set):
+    data_set = np.array(data_set)  # nd array
+    min_val = data_set.min(0)
+    max_val = data_set.max(0)
+    ranges = max_val - min_val  # range
+    norm_data_set = np.zeros(np.shape(data_set))  # initialize it with zeros
+    m = data_set.shape[0]  # m rows
+    norm_data_set = data_set - np.tile(min_val, (m, 1))
+    norm_data_set = norm_data_set / np.tile(ranges, (m, 1))
+    return norm_data_set, min_val, max_val
+
+matrix, class_array = file2matrix()
+normal_matrix, min_v, max_v = norm_data(matrix)
+inx = [1866, 2390, 2282]
+knn_classify(inx, matrix, None, None)
+# print(matrix)
+# print(min_v, max_v)
+# print(normal_matrix)
